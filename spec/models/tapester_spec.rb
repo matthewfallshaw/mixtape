1461fee89c26b64b291642387d72012dd97a50f3
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-06-13 17:14:48 +1000

    vacillating about interface

---
+++ spec/models/tapester_spec.rb
@@ -0,0 +1,13 @@
+require File.join(File.dirname(__FILE__), "../spec_helper")
+
+require "tapester.rb"
+
+describe Tapester do
+  describe "#tapes" do
+    it ""
+  end
+
+  describe "#empty_tapes" do
+    it ""
+  end
+end
