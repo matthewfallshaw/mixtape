48dba079c7229f9727c44cb7d86b0db78cb3c27e
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-06-16 19:30:18 +1000

    tapes show commitment, and links to improve it

--- app/helpers/tapes_helper.rb
+++ app/helpers/tapes_helper.rb
@@ -1,2 +1,15 @@
 module TapesHelper
+  def committed_tapesters
+    @tape.tapesters.count == 0 ? "<span class='embarrassing'>- none -</span>".html_safe : @tape.tapesters.collect {|tapester| link_to(tapester.name, tapester) }.to_sentence.html_safe
+  end
+  def uncommitted_tapesters
+    uctrs = Tapester.all - @tape.tapesters
+    begin
+      if (uctrs.size == 0)
+               "- none - I love it when a plan comes together -".html_safe
+      else
+        uctrs.collect {|tapester| link_to(tapester.name, tapester) }.to_sentence
+      end
+    end.html_safe
+  end
 end
